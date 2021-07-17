f7d96577033aa0d4dcb3d680c9028459aeccfa75
LabGuy94 <37680107+LabGuy94@users.noreply.github.com>
2021-07-17 14:48:33 +0300

    Create main.py

---
+++ proxychecker/main.py
@@ -0,0 +1,35 @@
+import requests
+import threading
+import random
+proxies = open('proxies.txt','r').read().splitlines()
+proxies = [{'https':'http://'+proxy} for proxy in proxies]
+checkedproxies = []
+timeouttime = 3
+threadsamount = 100
+def checkproxies():
+    for proxy in proxies:
+        global checkedproxies
+        if(not proxy in checkedproxies):
+            try:
+                randomproxy = random.choice(proxies)
+                checkedproxies.append(randomproxy)
+                r = requests.get('https://httpbin.org/ip', proxies=randomproxy, timeout=timeouttime)
+                print("working")
+                o = open("validproxies.txt","a+")
+                o.write(f"{randomproxy['https'].replace('http://', '')}\n")
+            except:
+                checkedproxies.append(randomproxy)
+                print("failed") 
+threads = []
+
+for i in range(threadsamount):
+    t = threading.Thread(target=checkproxies)
+    t.daemon = True
+    threads.append(t)
+
+for i in range(threadsamount):
+    threads[i].start()
+
+for i in range(threadsamount):
+    threads[i].join()
+print("Done")
