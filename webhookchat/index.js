92f7fe5664be15980ea422de423e8d1be047e59f
LabGuy94 <37680107+LabGuy94@users.noreply.github.com>
2021-08-19 19:08:56 -0700

    Update index.js

--- webhookchat/index.js
+++ webhookchat/index.js
@@ -1,1 +1,13 @@
-
+const mineflayer = require('mineflayer')
+const { Webhook } = require('discord-webhook-node');
+require('dotenv').config()
+const hook = new Webhook(process.env.WEBHOOK);
+const bot = mineflayer.createBot({
+    host: 'localhost', // minecraft server ip
+    username: 'bot1', // minecraft username
+})
+bot.on('chat', async (username, message) => {
+    hook.setUsername(username);
+    await hook.setAvatar("https://www.mc-heads.net/avatar/" + username);
+    await hook.send(message);
+})
